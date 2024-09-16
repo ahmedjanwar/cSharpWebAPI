a780beb40cc3766819130da4e0ae0700acc0cc74
ahmedjanwar <ajemal840@gmail.com>
2024-09-16 13:27:53 +0300

    dev branch

--- WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+966550d196eacb2ba02d28e86aa459d3f2da01e8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f181f618595b5da9b9a84eeee932227d2ae8e584")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
